cb421e251f972557708115bc30a14a6aafdfcfaa
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-03 07:18:59 +0200

    buttons works

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
+import { useState } from 'react';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/bootstrap.darkly.min.css';
 // import './styles/bootstrap.morph.min.css';
 // import './styles/bootstrap.quartz.min.css';
 import { Container, Button, Card } from 'react-bootstrap';
 import { Employees } from './components/Employees';
+import { Customers } from './components/Customers';
 
 function App() {
+	const [currentPage, setCurrentPage] = useState('customers');
 	return (
 		<div className="App">
 			<Container className="mt-5">
 				<h1>Company Site</h1>
-				<Button className="me-1">Employees</Button>
-				<Button className="btn-success me-1">Customers</Button>
+				<Button
+					onClick={() => setCurrentPage('employees')}
+					className="me-1"
+				>
+					Employees
+				</Button>
+				<Button
+					onClick={() => setCurrentPage('customers')}
+					className="btn-success me-1"
+				>
+					Customers
+				</Button>
 				<Card className="mt-4">
 					<Card.Body>
-						<Employees/>
+						{currentPage == 'employees' && <Employees />}
+						{currentPage == 'customers' && <Customers />}
 					</Card.Body>
 				</Card>
 			</Container>
---
+++ src/components/Customers.jsx
@@ -0,0 +1,8 @@
+export const Customers = () => {
+	return (
+		<>
+			<h2>Customers</h2>
+			<p>This is the customers page.</p>
+		</>
+	);
+};
