2bb5b4cf4e19586628f28507daea8eb81b18c141
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-03 07:01:38 +0200

    employees component in

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,24 @@
 import './styles/bootstrap.darkly.min.css';
 // import './styles/bootstrap.morph.min.css';
 // import './styles/bootstrap.quartz.min.css';
-import { Container, Button } from 'react-bootstrap';
+import { Container, Button, Card } from 'react-bootstrap';
+import { Employees } from './components/Employees';
 
 function App() {
-    return (
-        <div className="App">
-            <Container className="mt-5">
-                <h1>Company Site</h1>
-                <Button className="me-1">Employees</Button>
-                <Button className="btn-success me-1">Customers</Button>
-            </Container>
-        </div>
-    );
+	return (
+		<div className="App">
+			<Container className="mt-5">
+				<h1>Company Site</h1>
+				<Button className="me-1">Employees</Button>
+				<Button className="btn-success me-1">Customers</Button>
+				<Card className="mt-4">
+					<Card.Body>
+						<Employees/>
+					</Card.Body>
+				</Card>
+			</Container>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Employees.jsx
@@ -0,0 +1,8 @@
+export const Employees = () => {
+	return (
+		<>
+			<h2>Employees</h2>
+			<p>This is the employees page.</p>
+		</>
+	);
+};
